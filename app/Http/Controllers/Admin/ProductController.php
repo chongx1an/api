a7487260670e6256729c5d8185a85166e26c6c17
Chong <ch0ngx1an@hotmail.com>
2022-08-20 15:47:37 +0000

    add delete product endpoint

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -76,6 +76,10 @@ class ProductController extends Controller
      */
     public function delete($id)
     {
-        //
+        $product = Product::findOrFail($id);
+
+        $product->delete();
+
+        return response()->json(null);
     }
 }
